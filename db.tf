e045b9b6938c7c4ceb8af54bedee7a82a109daef
Daghan Kurtay Altunsoy <daghan.altunsoy@gmail.com>
2025-05-13 13:12:41 +0300

    feat(terraform): Deploy postgres high availability

---
+++ db.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "db" {
+  chart         = "postgresql-ha"
+  name          = "paysera"
+  namespace     = "paysera"
+  repository    = "oci://registry-1.docker.io/bitnamicharts"
+  version       = "16.0.3"
+  wait          = true
+  wait_for_jobs = true
+
+  values = [yamlencode(local.var.database)]
+
+  depends_on = [
+    kubernetes_namespace.paysera,
+  ]
+}
